e2dca825e0dff6b6d543e0e088202b3d8ce50dee
zhandtl <804661923@qq.com>
2016-10-17 19:55:44 +0800

    LanuchScreen
    LanuchScreen动画更改

---
+++ LaunchScreenAnimation-Storyboard-master/LaunchScreenAnimation-Storyboard/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  LaunchScreenAnimation-Storyboard
+//
+//  Created by ShiCang on 16/3/28.
+//  Copyright © 2016年 Cave. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self launchAnimation];
+}
+
+#pragma mark - Private Methods
+- (void)launchAnimation {
+    UIViewController *viewController = [[UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil] instantiateViewControllerWithIdentifier:@"LaunchScreen"];
+    
+    UIView *launchView = viewController.view;
+    UIWindow *mainWindow = [UIApplication sharedApplication].keyWindow;
+    launchView.frame = [UIApplication sharedApplication].keyWindow.frame;
+    [mainWindow addSubview:launchView];
+    
+    [UIView animateWithDuration:1.0f delay:0.5f options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        launchView.alpha = 0.0f;
+        launchView.layer.transform = CATransform3DScale(CATransform3DIdentity, 2.0f, 2.0f, 1.0f);
+    } completion:^(BOOL finished) {
+        [launchView removeFromSuperview];
+    }];
+}
+
+@end
